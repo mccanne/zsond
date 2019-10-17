6cc9ed303ca570653694523ed6727f0a999150c9
Steve McCanne <steve@mccanne.com>
2019-10-16 19:54:23 -0700

    Initial stab (#1)
    initial, barebones implementation

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # zsond
+
+zsond is a simple test server for receiving zson over http.  It accepts zson
+zeek files via HTTP POST, looks for the #path directive, and writes each file
+under current directory of the running zsond using the path given in the POST endpoint naming
+the log file and the path type.  If the file already exists, a different name
+is used by embedding a version number in the file.
+
+For example, run the server on port 9999:
+```
+git clone https://github.com/mccanne/zsond.git
+cd zsond
+go build
+mkdir logs
+./zsond :9999
+```
+Then, point zeek's [http-over-zson plugin]
+(https://github.com/looky-cloud/zson-http-plugin)
+ at the server and run zeek.
+You should see zeek logs appear in the server directory.
+
+You can manually push a log into zsond with curl, e.g., to push conn.log:
+```
+curl -X POST "http://localhost:9999/logs" --data-binary @conn.log
+```
+You need --data-binary here as curl will otherwise strip newlines
+from the log file.
+
+## bugs
+
+Buffering doesn't work very well.  Data will be held in memory until
+the buffer fills or zeek closes the http connection.
+
+SIGTERM isn't caught so if you ctrl-C zsond, you will lose any data
+buffered in memory.
---
+++ zsond.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func parsePath(path string) (string, error) {
+	dirpath := filepath.Join(".", path)
+	if _, err := os.Stat(dirpath); os.IsNotExist(err) {
+		return "", err
+	}
+	return dirpath, nil
+}
+
+func findPath(buf []byte) string {
+	for len(buf) > 5 {
+		off := bytes.IndexByte(buf, byte('\n'))
+		if off < 0 {
+			return ""
+		}
+		line := string(buf[:off])
+		if strings.HasPrefix(line, "#path") {
+			return strings.TrimSpace(line[5:])
+		}
+		buf = buf[off+1:]
+	}
+	return ""
+}
+
+func openfile(path, name string) (*os.File, string, error) {
+	for n := 0; n < 100; n++ {
+		var fname string
+		if n > 0 {
+			fname = fmt.Sprintf("%s.%d.log", name, n)
+		} else {
+			fname = fmt.Sprintf("%s.log", name)
+		}
+		filename := filepath.Join(path, fname)
+		f, err := os.OpenFile(filename, os.O_EXCL|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			if os.IsExist(err) {
+				continue
+			}
+			return nil, "", err
+		}
+		return f, filename, nil
+	}
+	return nil, "", errors.New("too many files")
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "bad method", http.StatusForbidden)
+		return
+	}
+	dirPath, err := parsePath(r.URL.RequestURI())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+	size := 128 * 1024
+	reader := bufio.NewReaderSize(r.Body, size)
+	buf, err := reader.Peek(size)
+	if err != nil && err != io.EOF {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+	if len(buf) == 0 {
+		http.Error(w, "empty body", http.StatusForbidden)
+		return
+	}
+	logType := findPath(buf)
+	if logType == "" {
+		http.Error(w, "no #path directive found", http.StatusForbidden)
+		return
+	}
+	file, name, err := openfile(dirPath, logType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if _, err := io.Copy(file, reader); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("wrote %s\n", name)
+}
+
+func main() {
+	port := ":9867"
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+	}
+	http.HandleFunc("/", handle)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
